6d84f22fffa4e378994a9db555f4355e6f05a820
Nicholas Esposito <nesposito@ufl.edu>
2023-06-06 09:18:16 -0400

    Initial script for cleaning Yakkertech data

---
+++ Cleaning-Frontier-Data.R
@@ -0,0 +1,12 @@
+## Cleaning Yakkertech data for upload to Frontier League shared drive
+
+
+# Pull Yakkertech data from internal drive --------------------------------
+
+
+# Clean data --------------------------------------------------------------
+
+
+# Upload to Frontier League shared drive ----------------------------------
+
+
